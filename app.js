f1a26371a3dcc6d8e09632cec19cddaa39dfa9d9
Jagruti Chitte <81913740+Jagruti2592@users.noreply.github.com>
2024-09-25 01:24:55 -0400

    update app.ts file with string, number and boolean datatypes

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-console.log('Hello World');
-console.log('This is my first typescript code');
+// 
+/* Variable in Typescript */
+//number datatype -has integer and float values
+var num = 10; //value of number datatype can be changed later in code
+console.log(num);
+var pi = 3.14; //fixed value and has to initialize at the time of declaration.
+console.log(pi);
+var num1 = 10; //fixed value and has to initialize at the time of declaration.
+console.log(num1);
+//string datatype
+var str = "Hello";
+console.log(str);
+// const str1 = 'this is a string created  
+//               using back ticks ${str};
+//               this is another line';
+// console.log(str1);
+//boolean datatype
+var bool = true;
+console.log(bool);
+var isEligible = false;
+console.log(isEligible);
+var isEquals = true;
+console.log(isEquals);
+//array datatype
+var arr = [1, 2, 3];
+console.log(arr);
--- app.ts
+++ app.ts
@@ -1,2 +1,36 @@
-console.log('Hello World');
-console.log('This is my first typescript code');
+// 
+
+/* Variable in Typescript */
+
+//number datatype -has integer and float values
+let num: number = 10; //value of number datatype can be changed later in code
+console.log(num);
+
+const pi=3.14; //fixed value and has to initialize at the time of declaration.
+console.log(pi);
+
+const num1: number = 10; //fixed value and has to initialize at the time of declaration.
+console.log(num1);
+
+//string datatype
+const str: string = "Hello";
+console.log(str);
+
+// const str1 = 'this is a string created  
+//               using back ticks ${str};
+//               this is another line';
+// console.log(str1);
+
+//boolean datatype
+let bool: boolean = true;
+console.log(bool);
+
+let isEligible: boolean = false;
+console.log(isEligible);
+
+let isEquals: boolean = true;
+console.log(isEquals);
+
+//array datatype
+let arr: number[] = [1, 2, 3];
+console.log(arr);
